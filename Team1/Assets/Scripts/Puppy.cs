46d981b3874926be351f580146ea270c6a0c34d5
Matjaž Ciglič <matjaz.ciglic.jst@gmail.com>
2023-09-26 11:36:39 +0200

    Add basic puppy movements

--- Team1/Assets/Scripts/Puppy.cs
+++ Team1/Assets/Scripts/Puppy.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Puppy : MonoBehaviour
 {
 
     public Transform player; // Reference to the player's Transform
     public float movementSpeed = 3.0f; // Adjust this to control the puppy's movement speed
+
+    public bool isFree = true;
 
     void Update() {
         if (player != null) {
@@ -16,13 +19,32 @@ public class Puppy : MonoBehaviour
 
 
     void Movement() {
-        // Calculate the direction from the puppy to the player
-        Vector3 moveDirection = transform.position - player.position;
+        if(isFree) {
+            // Calculate the direction from the puppy to the player
+            Vector3 moveDirection = transform.position - player.position;
 
-        // Normalize the direction vector to make the movement consistent
-        moveDirection.Normalize();
+            Vector3 randomness = Random.insideUnitSphere;
+            randomness.y = 0;
+            moveDirection += randomness;
 
-        // Move the puppy in the opposite direction of the player
-        transform.Translate(moveDirection * movementSpeed * Time.deltaTime);
+            // Normalize the direction vector to make the movement consistent
+            moveDirection.Normalize();
+
+
+            // find location where it wants to move
+            var whereToGo = transform.position + moveDirection * movementSpeed * Time.deltaTime;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(whereToGo, out hit, 100f, NavMesh.AllAreas)) {
+                // move
+                transform.position = hit.position;
+            } else {
+                // stop
+                Debug.Log("Puppy edge reached");
+            }
+
+        } else {
+            transform.Translate(player.position);
+            isFree = true;
+        }
     }
 }
