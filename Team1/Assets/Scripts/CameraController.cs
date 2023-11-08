21a8460f17bce37b83a6c859ea7f30bcf10e3c5e
Matjaz <matjaz.ciglic.jst@gmail.com>
2023-11-08 14:31:28 +0100

    Improved camera movement by interpolation

--- Team1/Assets/Scripts/CameraController.cs
+++ Team1/Assets/Scripts/CameraController.cs
@@ -7,11 +7,15 @@ public class CameraController : MonoBehaviour
     public Transform player;
     public Transform root;
 
+    public float minMovementRequired;
+    public float interpolationStep;
+
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if(player) {
-            root.position = player.transform.position;
+        if(player && Vector3.Distance(root.position, player.position) > minMovementRequired) {
+            // root.position = player.transform.position;
+            root.position = Vector3.Lerp(root.position, player.position, interpolationStep);
         }
     }
 }
